77bcd52ca3e205cea1ea7addaabe7507328101be
Techtumbler <jens.farronato@techtunmbler.ch>
2025-09-08 00:17:22 +0200

    Fix LSVA route filename case again

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import App from './App.jsx'
 import Timer from './routes/Timer.jsx'
 import Map from './routes/Map.jsx'
 import Docs from './routes/Docs.jsx'
-import Lsva from './routes/lsva.jsx'
+import Lsva from './routes/Lsva.jsx'
+// reimport to top up errror
 
 const router = createBrowserRouter([
   { path: '/', element: <App/>, children: [
