375c935b665c4398e90792ce0160a74691bfaa19
Techtumbler <jens.farronato@techtunmbler.ch>
2025-09-07 23:26:48 +0200

    App Init

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
+// src/main.jsx
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './styles/app.css'
 import App from './App.jsx'
+import Timer from './routes/Timer.jsx'
+import Map from './routes/Map.jsx'
+import Docs from './routes/Docs.jsx'
+import Lsva from './routes/Lsva.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  { path: '/', element: <App/>, children: [
+    { index: true, element: <Timer/> },
+    { path: 'map', element: <Map/> },
+    { path: 'docs', element: <Docs/> },
+    { path: 'lsva', element: <Lsva/> }
+  ]}
+])
+
+createRoot(document.getElementById('root')).render(<RouterProvider router={router}/>)
